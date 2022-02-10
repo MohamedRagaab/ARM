7d3077c8f829903bbbeb41e3aaca66a85ac865cc
Mohamed <mrfa221999@gmail.com>
2022-02-10 02:34:42 +0200

    first commit

---
+++ system/01-LIB/01-STD_TYPES/STD_TYPES.h
@@ -0,0 +1,14 @@
+/***************************************************************************************************************/
+/* Author : Mohamed Ragab                                                                                      */
+/* Date : 2/8/2022                                                                                             */
+/* Version : v01                                                                                               */
+/***************************************************************************************************************/
+
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char       u8;
+typedef unsigned short int  u16;
+typedef unsigned long int   u32;
+
+#endif
